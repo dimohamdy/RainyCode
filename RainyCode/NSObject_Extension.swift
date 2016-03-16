0fe8746413fcdc36b76440e125140e57a1a06e83
dimohamdy <dimohamdy>
2016-03-16 18:19:13 +0200

    Initial Commit

---
+++ RainyCode/NSObject_Extension.swift
@@ -0,0 +1,19 @@
+//
+//  NSObject_Extension.swift
+//
+//  Created by binaryboy on 3/16/16.
+//  Copyright © 2016 AhmedHamdy. All rights reserved.
+//
+
+import Foundation
+
+extension NSObject {
+    class func pluginDidLoad(bundle: NSBundle) {
+        let appName = NSBundle.mainBundle().infoDictionary?["CFBundleName"] as? NSString
+        if appName == "Xcode" {
+        	if sharedPlugin == nil {
+        		sharedPlugin = RainyCode(bundle: bundle)
+        	}
+        }
+    }
+}
---
+++ RainyCode/RainyCode.swift
@@ -0,0 +1,49 @@
+//
+//  RainyCode.swift
+//
+//  Created by binaryboy on 3/16/16.
+//  Copyright © 2016 AhmedHamdy. All rights reserved.
+//
+
+import AppKit
+
+var sharedPlugin: RainyCode?
+
+class RainyCode: NSObject {
+
+    var bundle: NSBundle
+    lazy var center = NSNotificationCenter.defaultCenter()
+
+    init(bundle: NSBundle) {
+        self.bundle = bundle
+
+        super.init()
+        center.addObserver(self, selector: Selector("createMenuItems"), name: NSApplicationDidFinishLaunchingNotification, object: nil)
+    }
+
+    deinit {
+        removeObserver()
+    }
+
+    func removeObserver() {
+        center.removeObserver(self)
+    }
+
+    func createMenuItems() {
+        removeObserver()
+
+        var item = NSApp.mainMenu!!.itemWithTitle("Edit")
+        if item != nil {
+            var actionMenuItem = NSMenuItem(title:"Do Action", action:"doMenuAction", keyEquivalent:"")
+            actionMenuItem.target = self
+            item!.submenu!.addItem(NSMenuItem.separatorItem())
+            item!.submenu!.addItem(actionMenuItem)
+        }
+    }
+
+    func doMenuAction() {
+        let error = NSError(domain: "Hello World!", code:42, userInfo:nil)
+        NSAlert(error: error).runModal()
+    }
+}
+
